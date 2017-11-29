a5a40af3f8e5730707c0419be99d767ad3a4accd
9501252 <9501252@1ITQ1A547LTC.KMX.LOCAL>
2017-11-29 16:17:33 -0500

    Third Commit

---
+++ ParameterisedCucumberSelenium/target/cucumber-pretty/report.js
@@ -0,0 +1,289 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Feature\\ParameterisedFeature.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Test Carmax Smoke Scenario",
+  "description": "",
+  "id": "test-carmax-smoke-scenario",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Test Search with Make Value",
+  "description": "",
+  "id": "test-carmax-smoke-scenario;test-search-with-make-value;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 3,
+  "name": "Open Chrome and start application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "I enter \"Honda \" and \"Civic\" in search box",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "user should able to see results accordingly",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 6,
+  "name": "Application should be closed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Open_Chrome_and_start_application()"
+});
+formatter.result({
+  "duration": 12752989436,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Honda ",
+      "offset": 9
+    },
+    {
+      "val": "Civic",
+      "offset": 22
+    }
+  ],
+  "location": "ParamsCucumberTest.I_enter_and_in_search_box(String,String)"
+});
+formatter.result({
+  "duration": 1270564341,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.user_should_able_to_see_results_accordingly()"
+});
+formatter.result({
+  "duration": 1093384844,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Application_should_be_closed()"
+});
+formatter.result({
+  "duration": 7138613450,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "Test Search with Make Value",
+  "description": "",
+  "id": "test-carmax-smoke-scenario;test-search-with-make-value;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 3,
+  "name": "Open Chrome and start application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "I enter \"Toyota \" and \"Camry\" in search box",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "user should able to see results accordingly",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 6,
+  "name": "Application should be closed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Open_Chrome_and_start_application()"
+});
+formatter.result({
+  "duration": 8213760853,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Toyota ",
+      "offset": 9
+    },
+    {
+      "val": "Camry",
+      "offset": 23
+    }
+  ],
+  "location": "ParamsCucumberTest.I_enter_and_in_search_box(String,String)"
+});
+formatter.result({
+  "duration": 1294992871,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.user_should_able_to_see_results_accordingly()"
+});
+formatter.result({
+  "duration": 1127670840,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Application_should_be_closed()"
+});
+formatter.result({
+  "duration": 7151998367,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 12,
+  "name": "Test Search with Make Value",
+  "description": "",
+  "id": "test-carmax-smoke-scenario;test-search-with-make-value;;4",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 3,
+  "name": "Open Chrome and start application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "I enter \"Honda \" and \"Accord\" in search box",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "user should able to see results accordingly",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 6,
+  "name": "Application should be closed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Open_Chrome_and_start_application()"
+});
+formatter.result({
+  "duration": 8488394124,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Honda ",
+      "offset": 9
+    },
+    {
+      "val": "Accord",
+      "offset": 22
+    }
+  ],
+  "location": "ParamsCucumberTest.I_enter_and_in_search_box(String,String)"
+});
+formatter.result({
+  "duration": 1204403298,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.user_should_able_to_see_results_accordingly()"
+});
+formatter.result({
+  "duration": 1134474146,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Application_should_be_closed()"
+});
+formatter.result({
+  "duration": 7098338736,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "Test Search with Make Value",
+  "description": "",
+  "id": "test-carmax-smoke-scenario;test-search-with-make-value;;5",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 3,
+  "name": "Open Chrome and start application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "I enter \"Ford \" and \"Focus\" in search box",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "user should able to see results accordingly",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 6,
+  "name": "Application should be closed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Open_Chrome_and_start_application()"
+});
+formatter.result({
+  "duration": 8836833315,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Ford ",
+      "offset": 9
+    },
+    {
+      "val": "Focus",
+      "offset": 21
+    }
+  ],
+  "location": "ParamsCucumberTest.I_enter_and_in_search_box(String,String)"
+});
+formatter.result({
+  "duration": 1195070174,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.user_should_able_to_see_results_accordingly()"
+});
+formatter.result({
+  "duration": 1109861841,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ParamsCucumberTest.Application_should_be_closed()"
+});
+formatter.result({
+  "duration": 7142478915,
+  "status": "passed"
+});
+});
